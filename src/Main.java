69fe7ac1dc489749a391b7e2abc919d2aafc84d6
LeanoraVilte <tina.ww.lea@gmail.com>
2024-05-31 15:12:29 +0300

    rtyuiopp45662

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         System.out.println("Hello world!");
 
         int b = 1;
-        int d = 100;
+        int d = 10;
         int c = 7;
 
-        System.out.print(b/d + c);
+        System.out.print(b*d + c);
     }
 }
