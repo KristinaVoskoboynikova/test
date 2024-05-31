1cda093d089b2d309df9ee10074e0a8231d8e98e
LeanoraVilte <tina.ww.lea@gmail.com>
2024-05-31 15:10:58 +0300

    rtyuioppppppppppppppppppppppppppppppppppp

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int b = 1;
+        int d = 100;
+        int c = 7;
+
+        System.out.print(b/d + c);
+    }
+}
